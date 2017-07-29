788edf1e124f340e642f476a4738c13e469a98cd
marcionitao <marcionitao@gmail.com>
2017-07-29 12:58:16 +0200

    add bootstrap, gitPages, logo image, background style, coins component and coins service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
+import { CoinsService } from './service/coins.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CoinsComponent } from './coins/coins.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoinsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [CoinsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
